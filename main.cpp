539aff77660f051a3c5dab263f1fd5100da590b8
Hana Kim <101982606+hanagertrudekim@users.noreply.github.com>
2023-03-07 09:02:53 +0000

    hw2 - 1 solve code

--- main.cpp
+++ main.cpp
@@ -1,31 +1,52 @@
-#include <array>
+#include <cstring>
 #include <iostream>
 using namespace std;
 
-void Swap(int &a, int &b) {
-  int temp = a;
-  a = b;
-  b = temp;
-}
+class Person {
+private:
+  char name[20] = {0};
+  int id;
+  int height;
 
-void BubbleSort(int *arrIn, int nArr) {
-  for (int i = 0; i < nArr; i++) {
-    for (int j = i+1; j < nArr; j++) {
-      if (arrIn[i] < arrIn[j]) {
-        Swap(arrIn[i], arrIn[j]);
-      } else {
-        continue;
-      }
-    }
+public:
+  char *getName() { return name; }
+
+  int getID() { return id; }
+
+  int getHeight() { return height; }
+
+  void printStudentInfo() {
+    cout << " ID: " << id << " name: " << name << " height: " << height << endl;
   }
+
+  void init(int ID, const char *Name, int Height) {
+    id = ID;
+    strcpy(name, Name);
+    height = Height;
+  }
+};
+
+void personSwap(Person a[2]) {
+  struct Person temp;
+  temp.init(a[0].getID(), a[0].getName(), a[0].getHeight());
+  a[0].init(a[1].getID(), a[1].getName(), a[1].getHeight());
+  a[1].init(temp.getID(), temp.getName(), temp.getHeight());
 }
 
 int main() {
-  int arrIn[12] = {5, 11, 6, 2, 3, 1, 9, 12, 7, 8, 10, 4};
+  struct Person student[2];
 
-  BubbleSort(arrIn, (sizeof(arrIn) / sizeof(int)));
+  student[0].init(20201125, "wnsh", 33);
+  student[1].init(2020132, "hana", 33);
 
-  for (int i : arrIn) {
-    cout << i << " ";
+  for (int i = 0; i < 2; i++) {
+    student[i].printStudentInfo();
+  }
+
+  cout << "값 교환 후" << endl;
+  personSwap(student);
+
+  for (int i = 0; i < 2; i++) {
+    student[i].printStudentInfo();
   }
 }
